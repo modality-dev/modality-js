1cf355d9715133b4c82137fb0ecf73c388f97b5b
rarepepi <pepimartinez@protonmail.com>
2024-07-12 01:08:32 -0400

    remove api so at least can see lander

--- packages/network-explorer/app/page.tsx
+++ packages/network-explorer/app/page.tsx
@@ -2,15 +2,15 @@ import { Lander } from "@/components/main";
 
 export default async function Page() {
   const baseUrl = "http://localhost:3000";
-  const res = await fetch(`${baseUrl}/api`, {
-    headers: {
-      "x-datastore": "../../network-cli/tmp/datastore",
-    },
-  });
-  const data = await res.json();
+  // const res = await fetch(`${baseUrl}/api`, {
+  //   headers: {
+  //     "x-datastore": "../../network-cli/tmp/datastore",
+  //   },
+  // });
+  // const data = await res.json();
   return (
     <main className="flex flex-col items-center justify-center">
-      <Lander status={data.status} round={23} />
+      <Lander status={"ready"} round={23} />
     </main>
   );
 }
