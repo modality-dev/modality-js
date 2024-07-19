f7f5a3a192b8e918bb42f178ee8578b0772132fa
rarepepi <pepimartinez@protonmail.com>
2024-07-19 04:10:28 -0400

    add port to network-viewer listening console log

--- packages/network-viewer/api/index.mjs
+++ packages/network-viewer/api/index.mjs
@@ -1,5 +1,5 @@
 import { setupServer } from "@thylacine-js/webapi-express";
-import NetworkDatastore from '@modality-dev/network-datastore';
+import NetworkDatastore from "@modality-dev/network-datastore";
 import NetworkDatastoreBuilder from "@modality-dev/network-datastore/NetworkDatastoreBuilder";
 
 import DAGRider from "@modality-dev/network-consensus/sequencing/DAGRider";
@@ -11,12 +11,12 @@ import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const appDir = `${__dirname}/`;
 
-export default async function main({port, datastore}) {
+export default async function main({ port, datastore }) {
   port = port || 3001;
 
   const server = await setupServer({ appDir, validateCors: () => true });
 
-  if (datastore === 'mock') {
+  if (datastore === "mock") {
     const SCRIBES = 5;
     const ROUNDS = 12;
     const builder = await NetworkDatastoreBuilder.createInMemory();
@@ -40,8 +40,8 @@ export default async function main({port, datastore}) {
     server.datastore = await NetworkDatastore.createInMemory();
   }
   server.listen(port, () => {
-    console.log(`listening on http://0.0.0.0:3001`);
-  }); 
+    console.log(`listening on http://0.0.0.0:${port}`);
+  });
 }
 
 import cliCalls from "cli-calls";
