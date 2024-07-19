75904183d26a8947ccb7f1762dc2879b59e70071
rarepepi <pepimartinez@protonmail.com>
2024-07-19 04:40:24 -0400

    get drizzle data in server component

--- packages/network-explorer/app/page.tsx
+++ packages/network-explorer/app/page.tsx
@@ -1,7 +1,11 @@
 import { Lander } from "@/components/main";
 import RoundDetail from "@/components/round";
+import db from "@/db/drizzle";
+import { keyValues } from "@/db/schema";
 
 export default async function Page() {
+  const data = await db.select().from(keyValues);
+  console.log({ data });
   return (
     <main className="flex flex-col items-center justify-center">
       {/* <Lander status={""} round={3} /> */}
